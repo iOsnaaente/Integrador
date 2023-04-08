be19edd57368c12842d9fc28b8eef911d1f38950
Bruno Sampaio <bruno.bielsam.1205@hotmail.com>
2023-04-08 18:23:28 -0300

    Att server app - OK

--- server_app/database.py
+++ server_app/database.py
@@ -1,4 +1,8 @@
+import datetime 
 import sqlite3 
+import json
+import pytz 
+import os 
 
 class Database: 
     def __init__(self, path ) -> None:
@@ -7,227 +11,174 @@ class Database:
         self.cursor = self.con.cursor()
         self.create_tables() 
 
-    def create_tables( self ): 
-        # Cria tabela de familia 
+    def create_tables( self ):
+        # Cria tabela de gerenciador e nível de acesso
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS 
-                family( 
+                manager (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name VARCHAR(24) NOT NULL
+                    manager_group VARCHAR(32) NOT NULL,
+                    password VARCHAR(32) NOT NULL,
+                    level_access INTEGER NOT NULL
                 )
-            ''')
-        # Cria tabela de membros
+            '''             
+        )
+        # Cria tabela de usuários
         self.cursor.execute(''' 
             CREATE TABLE IF NOT EXISTS 
-                members(
+                user(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name VARCHAR(24) NOT NULL,
                     username VARCHAR(24) NOT NULL,
                     password VARCHAR(24) NOT NULL,
-                    family_id INTEGER NOT NULL,
-                    profit float,
+                    last_access date NOT NULL,
+                    name VARCHAR(24) NOT NULL,
                     photo blob,
-                    FOREIGN KEY (family_id) REFERENCES family (id)
-                )
-            '''
-        )
-        # Cria tabela Gasto
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS 
-                spendings (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    cat VARCHAR(50) NOT NULL,
-                    value FLOAT NOT NULL,
-                    date_buy DATE NOT NULL,
-                    member_id INTEGER NOT NULL,
-                    FOREIGN KEY (member_id) REFERENCES members (id)
+                    manager_id integer NOT NULL,
+                    FOREIGN KEY (manager_id) REFERENCES manager (id)
                 )
             '''
         )
         self.con.commit() 
 
-
-    def create_user( self, name : str, user : str, password : str, family_name : str, __debug : bool = False ) -> bool:
-        # Procura o ID de um usuário com o mesmo username 
-        self.cursor.execute( 'SELECT id FROM members WHERE username = ?', ( user, ) )
-        has_user = self.cursor.fetchall()
-        # Se encontrar um ID então já existe usuário com esse username
-        if has_user:
+    def create_new_user( self, manager_group : str, manager_psd : str, username : str, psd : str, name : str, __debug : bool = False ) -> bool:
+        # Primeiro verifica se o manager possui as credenciais 
+        self.cursor.execute( 'SELECT id FROM manager WHERE manager_group = ? AND password = ?', (manager_group, manager_psd, ) )
+        manager_id = self.cursor.fetchall() 
+        if not manager_id:
             if __debug:
-                print( f'Username already registered at {user}')
-            return False
-        # Caso contrário deve criar um novo
-        else:  
-            # Pega o ID da familia se ela existir 
-            self.cursor.execute( "SELECT id FROM family WHERE name = ?", ( family_name, ) )
-            family_id = self.cursor.fetchone() 
-            # Se não existe então cria uma nova familia 
-            if not family_id: 
+                print( 'Manager not found or password incorrect' )
+            return False 
+        else: 
+            # Procura o ID de um usuário com o mesmo username 
+            self.cursor.execute( 'SELECT id FROM user WHERE username = ?', ( username, ) )
+            has_user = self.cursor.fetchall()
+            # Se encontrar um ID então já existe usuário com esse username
+            if has_user:
                 if __debug:
-                    print(f'Family dont exist. Creating {family_name} family.')
-                self.cursor.execute( 'INSERT INTO family(name) VALUES (?)', (family_name, ) )
-                self.con.commit() 
-            # Pega o novo ID da familia 
-            self.cursor.execute( "SELECT id FROM family WHERE name = ?", ( family_name, ) )
-            family_id = self.cursor.fetchone() 
-            # Adiciona o novo membro dentro da tabela
-            self.cursor.execute( 'INSERT INTO members( name, username, password, family_id ) VALUES(?,?,?,?)', ( name, user, password, family_id[0]) )
+                    print( f'Username already registered at { username }')
+                return False
+            # Caso contrário deve criar um novo
+            else:  
+                last_access = self.get_date_now() 
+                manager_id = manager_id[0][0]
+                self.cursor.execute( 'INSERT INTO user(username, password, last_access, name, manager_id ) VALUES (?,?,?,?,?)', ( username, psd, last_access, name, manager_id ,) )
+                self.con.commit()
+                if __debug:
+                    print(f'Username {username} registered.')
+                return True 
+            
+    def create_new_manager( self, group : str, password : str, level_access : int, __debug : bool = False ):
+        # Procura o ID de um usuário com o mesmo username 
+        self.cursor.execute( 'SELECT id FROM manager WHERE manager_group = ?', ( group, ) )
+        has_manager = self.cursor.fetchall()
+        # Se encontrar um ID então já existe usuário com esse username
+        if has_manager:
+            if __debug:
+                print( f'Manager already registered like { group }' )
+            return False
+        # Caso contrario adionar um novo gerenciador
+        else: 
+            self.cursor.execute( 'INSERT INTO manager( manager_group, password, level_access ) VALUES (?,?,?)', ( group, password, level_access ,) )
             self.con.commit()
             if __debug:
-                print(f'Username {user} registered.')
-        return True
+                print(f'Manager {group} registered.')
+            return True             
 
-
-    def login( self, user : str, password : str, __debug : bool = False ) -> bool:
-        # Procura a senha do usuário cadastrado com o username 
-        self.cursor.execute( 'SELECT password FROM members WHERE username = ?', ( user, ) )
-        user_data = self.cursor.fetchall()
-        # Se não há nenhum usuário cadastrado, não retorna nada 
-        if not user_data: 
+    def login( self, username: str, password: str, __debug: bool = False) -> str:
+        # Pega os dados de usuário caso ele exista 
+        self.cursor.execute('''
+            SELECT user.*, manager.level_access 
+                FROM user 
+                    INNER JOIN manager ON user.manager_id = manager.id 
+                WHERE user.username = ? AND user.password = ?
+            ''', 
+            (username, password, ) 
+        )
+        result = self.cursor.fetchone()
+        # Verifica se foi encontrado um usuário ou não 
+        if not result:
             if __debug:
-                print( f'Não possui nenhum usuário cadastrado como {user}')
+                print(f"User {username} not found or password incorrect")
+            return json.dumps({"error" : "User not found or password incorrect" } )
+
+        user_dict = {
+            "id": result[0],
+            "username": result[1],
+            "password": result[2],
+            "last_access": result[3],
+            "name": result[4],
+            "photo": result[5].hex() if result[5] else None,
+            "manager_id" : result[6],
+            "level_access": result[7]
+        }
+        # Atualiza o ultimo acesso 
+        self.set_user_last_access( username ) 
+
+        if __debug:
+            print(f"User {username} logged in successfully")
+        # Retorna o Json 
+        return json.dumps(user_dict)
+
+    def get_date_now( self, utc : str = 'America/Sao_Paulo' ):
+        # pytz cria um objeto timezone nos moldes do datetime 
+        timezone = pytz.timezone( utc )
+        # Retorna no modelo YYYY-MM-DD usado pelo sqlite3
+        return datetime.datetime.now( tz = timezone ).strftime('%Y-%m-%d %H:%M:%S')
+        
+    def get_user_level_access( self, username : str, __debug : bool = False  ) -> int: 
+        # Procura o ID do manager dentro do usuário definido 
+        self.cursor.execute( 'SELECT manager_id FROM user WHERE username = ?', ( username, ) )
+        manager_id =  self.cursor.fetchall()
+        try:
+            # Após, basta procurar o level access do manager associado 
+            self.cursor.execute( 'SELECT level_access FROM manager WHERE id = ?', (manager_id[0][0],))
+            level_access = self.cursor.fetchall()
+            if __debug: 
+                print( f'[{username}] Manager {manager_id[0][0]} has {level_access[0][0]} level access')
+            return level_access[0][0]
+        except:
+            if __debug: 
+                print( f'Error. Username not found' )
             return False 
-        # Se não, basta comparar as senhas
-        if user_data[0][0] == password:
-            if __debug:   
-                print( f'Username {user} found and psd check' )
-            return True 
-        else:
-            if __debug:
-                print( f'Username {user} and psd dont check')
-            return False
-
-
-    def add_spending( self, username : str, value : float, type : str, date_buy : str, __debug : bool = False ) -> bool:
-        # Pega o ID do usuário 
-        self.cursor.execute( 'SELECT id FROM members WHERE username = ?', ( username, ) )
-        user_id = self.cursor.fetchall()
-        if not user_id: 
-            return False 
-        else: 
-            # Seleciona o valor dentro da lista 
-            user_id = user_id[0][0]
-            # Adiciona o gasto na lista de usuário
-            self.cursor.execute( 'INSERT INTO spendings( cat, value, data_buy, member_id ) VALUES (?,?,?,?)', ( type, value, date_buy, user_id ) )
-            self.con.commit() 
-            if __debug:
-                print( f'Spending registered: [{type}] R${value} at {date_buy}')
-            return True
-         
-
-    def get_family_profit( self, family_name : str, __debug : bool = False ) -> float:
-        # Inicia pegando o valor do ID da familia 
-        self.cursor.execute( 'SELECT id FROM family WHERE name = ? ', ( family_name, ) )
-        family_id = self.cursor.fetchall() 
-        # Se não existir familia, retorna false 
-        if not family_id:
-            if __debug:
-                print( f'{family_name} id not found')
-            return False 
-        else: 
-            # Se não, pega todos os salários dos membros da familia
-            family_id = family_id[0][0]
-            self.cursor.execute( 'SELECT profit FROM members WHERE family_id = ?', ( family_id, ) ) 
-            profits = self.cursor.fetchall() 
-            # Realiza a soma dos salários 
-            profit_sum = 0 
-            for profit in profits:
-                if profit[0]:
-                    profit_sum += profit[0]
-                else: 
-                    continue
-            return profit_sum
     
-    def get_total_family_spending( self, family_name : str, start_date : str = '', end_date : str = '', __debug : bool = False ) -> float:
-        # Inicia pegando o valor do ID da familia 
-        self.cursor.execute( 'SELECT id FROM family WHERE name = ? ', ( family_name, ) )
-        family_id = self.cursor.fetchall() 
-        # Se não existir familia, retorna false 
-        if not family_id:
-            if __debug:
-                print( f'{family_name} id not found')
+    def get_user_photo( self, username : str, __debug : bool = False ) -> bytearray:
+        # Procura a foto dentro do usuário definido 
+        self.cursor.execute( 'SELECT photo FROM user WHERE username = ?', ( username, ) )
+        photo =  self.cursor.fetchall()
+        if not photo:
+            if __debug: 
+                print( f'{username} dont have a photo updated. Can use datebase.att_photo method.')
             return False 
         else: 
-            # Se não, pega todos os IDs dos membros da familia
-            self.cursor.execute( 'SELECT id FROM members WHERE family_id = ?', ( family_id[0][0], ) ) 
-            members_ids = self.cursor.fetchall() 
-            spendings_sum = 0 
-            for member in members_ids:
-                # Realiza a soma dos gastos de cada membro da familia SEM delimitação de datas 
-                if not start_date:
-                    self.cursor.execute( 'SELECT value FROM spendings WHERE member_id = ?', (member[0], ) )
-                # Realiza a soma dos gastos de cada membro da familia COM delimitação de datas 
-                else: 
-                    self.cursor.execute( 'SELECT value FROM spendings WHERE date_buy BETWEEN ? AND ? AND member_id = ?', (start_date, end_date, member[0], ) )
-                spendings = self.cursor.fetchall() 
-                for spending in spendings:
-                    spendings_sum += spending[0] 
-            return spendings_sum 
-        
-
-    def get_member_spending( self, username : str, start_date : str = '', end_date : str = '', __debug : bool = False ) -> float:
-        self.cursor.execute( 'SELECT id FROM members WHERE username = ?', ( username,  ) )
-        member_id = self.cursor.fetchall() 
-        if not member_id:
-            if __debug:
-                print( f'{username} id not found')
-            return False 
-        else: 
-            spendings_sum = 0
-            # Realiza a soma dos gastos do usuário SEM delimitação de datas 
-            if not start_date:
-                self.cursor.execute( 'SELECT value FROM spendings WHERE member_id = ?', ( member_id[0], ) ) 
-            # Realiza a soma dos gastos do usuário COM delimitação de datas 
-            else: 
-                self.cursor.execute( 'SELECT value FROM spendings WHERE date_buy BETWEEN ? AND ? AND member_id = ?', (start_date, end_date, member_id[0], ) ) 
-            for spending in self.cursor.fetchall():
-                spendings_sum += spending[0]
-            return spendings_sum 
-        
-    def get_photo( self, username : str, __debug : bool = False ) -> bytearray:
-        try: 
-            self.cursor.execute( 'SELECT photo FROM members WHERE username = ?', ( username, ) )
-            photo =  self.cursor.fetchall()[0]
             if __debug: 
                 print( f'[{username}] A photo has found')
-            return photo
-        except:
-            print( f'{username} dont have a photo updated. Can use datebase.att_photo method.')
-            return False 
-        
+            return photo[0][0]
+         
+    def set_user_photo( self, username : str, photo : bytearray, __debug : bool = False ) -> bool:
+        # Apenas faz o UPDATE de photo no lugar onde há o username 
+        self.cursor.execute( 'UPDATE user SET photo = ? WHERE username = ?', ( sqlite3.Binary( photo ), username, ))
+        self.con.commit()
+        if __debug: 
+            print( f'Username {username} has a nem photo')
+        return True 
 
-    def att_profit( self, username : str, value : float, __debug : bool = False ) -> bool:
-        try: 
-            # Apenas faz o UPDATE do profit no lugar onde há o username 
-            self.cursor.execute( 'UPDATE members SET profit = ? WHERE username = ?', ( value, username, ))
-            self.con.commit()
-            if __debug: 
-                print( f'Username {username} has the nem profit {value}')
-            return True 
-        except:
-            if __debug: 
-                print( f'Not found the username {username}')
-            return False 
-        
+    def set_user_last_access( self, username : str, __debug : bool = False ) -> bool:
+        last_access = self.get_date_now() 
+        self.cursor.execute( 'UPDATE user SET last_access = ? WHERE username = ? ', (username, last_access, ) ) 
+        self.con.commit() 
+        if __debug: 
+            print( f'Username {username} updated last access: {last_access}')
+        return True 
 
-    def att_photo( self, username : str, photo : bytearray, __debug : bool = False ) -> bool:
-        try: 
-            # Apenas faz o UPDATE de photo no lugar onde há o username 
-            self.cursor.execute( 'UPDATE members SET photo = ? WHERE username = ?', ( photo, username, ))
-            self.con.commit()
-            if __debug: 
-                print( f'Username {username} has a nem photo')
-            return True 
-        except:
-            if __debug: 
-                print( f'Not found the username {username}')
-            return False 
-
-
+# Teste de uso 
 if __name__ == '__main__':
-    db = Database('db\\database_2.db') 
-    print( db.create_user( 'Bruno', 'ios', '123', 'Sampaio', True))
-    print( db.login( 'iosnaaente', '123' ) ) 
-    print( db.add_spending('iosnaaente', 150, 'food', '12/05/1999' )  )
-    db.att_profit( 'iosnaaente', 1500 )
-    print( db.get_family_profit( 'Sampaio', True ) )  
+    db = Database( os.path.dirname(__file__) + '/db/database.db') 
+    db.create_new_manager( 'sup', 'sup123', 10, True )
+    db.create_new_user( 'sup', 'sup123', 'iosnaaente', '1205', 'BrunoSup', True )
+    db.get_user_level_access( 'iosnaaente', True )
+    print( db.login( 'iosnaaente', '1205', True ) )
+    
+    db.create_new_manager( 'adm', 'adm123',  1, True )
+    db.create_new_user( 'adm', 'adm123', 'iOsnaaenteAdm', '1205', 'BrunoAdm', True )
+    db.get_user_level_access( 'iOsnaaenteAdm', True )
+    print( db.login( 'iOsnaaenteAdm', '1205', True ) )
